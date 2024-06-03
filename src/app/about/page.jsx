f456b44d8a05b69e39713a8580cb7b5f1bcdb9f2
unknown <florezf90@gmail.com>
2024-06-02 22:26:55 -0500

    designed basic layout on the navbar to define the page routing, this will allow me to make development more easily.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 
 export default function About() {
     return (
-      <main>
-        <h1>About</h1>
-        <Link href="/">Home</Link>
+      <main className="container bg-white text-black mx-auto">
+        <h1 className="text-5xl m-5 pt-6">About</h1>
+        <Link href="/" className="m-5"><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3 text-2xl ">Home</button></Link>
+        <Link href="/services" className="m-5"><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  rounded my-3 text-2xl ">Services</button></Link>
       </main>
     ); 
 }
