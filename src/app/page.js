5e688de6d08b410441632d9fd1d99dae71cf8373
unknown <florezf90@gmail.com>
2024-06-06 07:55:35 -0500

    applied fixed styling to the background img in the home about section, making it responsive to most used devices screen size

--- src/app/page.js
+++ src/app/page.js
@@ -14,23 +14,23 @@ export default function Home() {
           Where Fitness Becomes Your Lifestyle
         </h3>
       </section>
-      <section className=" flex flex-col md:flex-row justify-center align-center max-w-7xl mx-auto  my-40  ">
-        <div className=" p-10 halfcontainer">
-          <Image src={trainer} alt="gym" width={600} height={500} />
+      <section className=" flex flex-col lg:flex-row justify-center align-center max-w-7xl mx-auto  my-40  ">
+        <div className=" mx-auto  halfcontainer" aria-description="gymImg">
+          <div className="sideImg mx-auto "></div>
         </div>
-        <div className=" flex flex-col p-10 text-center halfcontainer align-center justify-center">
-          <h1 className="text-4xl font-bold tracking-tight text-white  my-10">About</h1>
-          <p className="text-2xl  tracking-tight text-white my-8 ">
+        <div className=" flex flex-col p-10 text-center halfcontainer   align-center justify-center">
+          <h1 className="text-4xl font-bold tracking-tight text-white ml-40 my-10">About us</h1>
+          <p className="text-2xl  tracking-tight text-white ml-40  my-8 ">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam est
             eaque impedit vitae! Voluptatem ratione asperiores harum, alias qui,
             totam voluptas deserunt enim vero quisquam eius numquam aperiam,
             consequuntur fugit! Repellat voluptate provident ullam.
           </p>
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-9 mb-60 text-2xl "
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-9 mb-60 text-2xl ml-40  "
             aria-label="learn more"
           >
-            Read More
+            Learn  More
           </button>
         </div>
       </section>
